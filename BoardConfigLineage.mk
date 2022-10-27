2ba255133b2d0e82a43bd0901bafe50127adafbc
c0nfid <beiov2004.mail@gmail.com>
2022-10-27 04:38:41 -0400

    De-lineage-ify

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -14,10 +14,6 @@ TARGET_KERNEL_CONFIG := floral_defconfig
 TARGET_KERNEL_SOURCE := kernel/google/msm-4.14
 TARGET_NEEDS_DTBOIMAGE := true
 
-# Manifests
-DEVICE_MANIFEST_FILE += device/google/coral/lineage_manifest.xml
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/lineage/config/device_framework_matrix.xml
-
 # Needed for camera
 TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE := true
 
@@ -25,20 +21,6 @@ TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE := true
 AB_OTA_PARTITIONS += \
     vendor
 BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
-
-# Reserve space for gapps install
-ifneq ($(WITH_GMS),true)
-BOARD_PRODUCTIMAGE_EXTFS_INODE_COUNT := -1
-BOARD_PRODUCTIMAGE_PARTITION_RESERVED_SIZE := 614400000
-BOARD_SYSTEMIMAGE_EXTFS_INODE_COUNT := -1
-BOARD_SYSTEMIMAGE_PARTITION_RESERVED_SIZE := 1118924800
-BOARD_SYSTEM_EXTIMAGE_EXTFS_INODE_COUNT := -1
-BOARD_SYSTEM_EXTIMAGE_PARTITION_RESERVED_SIZE := 92160000
-endif
-
-# SELinux
-BOARD_SEPOLICY_DIRS += device/google/coral/sepolicy-lineage/dynamic
-BOARD_SEPOLICY_DIRS += device/google/coral/sepolicy-lineage/vendor
 
 # Verified Boot
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
